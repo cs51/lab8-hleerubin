4407f708f0c6ec4abb1fb92f2dccdaa0be06e6b1
Harry Lee-Rubin <totaldorkist@Harrys-MacBook-Air.local>
2018-04-12 14:09:26 -0400

    somewhat more done

--- lab8.ml
+++ lab8.ml
@@ -212,7 +212,7 @@ Exercise 8: Create a new event called publish to signal that all
 stories should be published. The event should be a unit WEvent.event.
 ......................................................................*)
 
-let publish = fun _ -> failwith "publish not implemented" ;; 
+let publish : unit WEvent.event = WEvent.new_event () ;;
 
 (*......................................................................
 Exercise 9: Write a function receive_report to handle new news
